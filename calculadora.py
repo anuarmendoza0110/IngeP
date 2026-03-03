48c554e5ba8b402f4c39056ea03cd1fb24c955ff
anuarmendoza <anuarmendoza0110@gmail.com>
2026-03-02 17:20:23 -0800

    Cambio intencional para que falle la prueba de subtract

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,6 @@
 class Calculadora:
     def add(self, a, b):
         return a + b
-    def subtract(self,a,b):
-        return a - b
+
+    def subtract(self, a, b):
+        return a + b  # Cambio intencional para que falle la prueba
